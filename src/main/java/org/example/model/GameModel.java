0a0ad7ad7f18edc4e9e836b0fde966413aba41e3
badop <patryk.bado@ttpsc.pl>
2025-05-24 00:11:37 +0200

    GameModel class implemented

--- src/main/java/org/example/model/GameModel.java
+++ src/main/java/org/example/model/GameModel.java
@@ -6,16 +6,64 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
-@Setter
 @Getter
-@RequiredArgsConstructor
 public class GameModel {
-    @NonNull
-    private String homeTeam;
-    @NonNull
-    private String awayTeam;
-    private int homeTeamGoals = 0;
-    private int awayTeamGoals = 0;
-    private final LocalDateTime timestamp = LocalDateTime.now();
+    private final String homeTeam;
+    private final String awayTeam;
+    private int homeTeamGoals;
+    private int awayTeamGoals;
+    private final LocalDateTime timestamp;
+
+    public GameModel(String homeTeam, String awayTeam) {
+        validateTeamNames(homeTeam, awayTeam);
+
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeTeamGoals = 0;
+        this.awayTeamGoals = 0;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public void updateScore(int newHomeTeamGoals, int newAwayTeamGoals){
+        validateNewScore(newHomeTeamGoals, newAwayTeamGoals);
+
+        this.homeTeamGoals = newHomeTeamGoals;
+        this.awayTeamGoals = newAwayTeamGoals;
+    }
+
+    private void validateNewScore(int newHomeTeamGoals, int newAwayTeamGoals) {
+        if(newHomeTeamGoals < 0 || newAwayTeamGoals < 0){
+            throw new IllegalArgumentException("New score cannot be lower than zero!");
+        }
+        if(newHomeTeamGoals < this.homeTeamGoals || newAwayTeamGoals < this.awayTeamGoals){
+            throw new IllegalArgumentException("New score cannot be lower than existing one!");
+        }
+    }
+
+    private void validateTeamNames(String homeTeam, String awayTeam) {
+        if (homeTeam == null || awayTeam == null) {
+            throw new IllegalArgumentException("Home and away teams have to be present!");
+        }
+        if (homeTeam.isBlank() || awayTeam.isBlank()) {
+            throw new IllegalArgumentException("Home and away teams cannot be blank!");
+        }
+        if (homeTeam.equals(awayTeam)) {
+            throw new IllegalArgumentException("Home and away teams must be different!");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GameModel gameModel = (GameModel) o;
+        return Objects.equals(homeTeam, gameModel.homeTeam) && Objects.equals(awayTeam, gameModel.awayTeam);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(homeTeam, awayTeam);
+    }
 }
